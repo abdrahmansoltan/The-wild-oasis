470737fd13de9a0761cd7be1aa0a0218e76cd78c
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-10 12:48:25 +0300

    feat: :wrench: setup main routes in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
 import GlobalStyles from './styles/GlobalStyles';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-      <H1>The Wild Oasis</H1>
-    </>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+        </Route>
+
+        <Route path='login' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
