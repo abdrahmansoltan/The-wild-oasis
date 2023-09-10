99638eff734b95a71940bec0caf3985cdebcae17
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-10 12:18:39 +0300

    style: :lipstick: add global styles and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>The Wild Oasis</H1>
+    </>
+  );
 }
 
 export default App;
