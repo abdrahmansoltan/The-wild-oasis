c8aa7a64b18afa2d9f89d749ee45c4a38f0787fe
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-22 11:08:09 +0300

    feat: ✨ Add client-side sorting operations to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -17,6 +17,12 @@ function CabinTable() {
   if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0);
   if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0);
 
+  // Handle sort by price FE-side
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [sortField, sortDirection] = sortBy.split('-');
+  const sortModifier = sortDirection === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a, b) => (a[sortField] - b[sortField]) * sortModifier); // * sortModifier to reverse sort direction for 'desc' sortDirection
+
   return (
     <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr' empty='No cabins found'>
       <Table.Header>
@@ -29,10 +35,7 @@ function CabinTable() {
       </Table.Header>
 
       {/* Render Props Pattern */}
-      <Table.Body
-        data={filteredCabins}
-        render={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
-      />
+      <Table.Body data={sortedCabins} render={cabin => <CabinRow key={cabin.id} cabin={cabin} />} />
     </Table>
   );
 }
