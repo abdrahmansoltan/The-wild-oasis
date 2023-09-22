15751e4b43cbbae1837a22db82b5a290e547a8aa
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-22 09:58:52 +0300

    feat: :sparkles: Add client-side filtering operations to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'react-router-dom';
 import Spinner from '../../ui/Spinner';
 import Table from '../../ui/Table';
 import CabinRow from './CabinRow';
@@ -5,8 +6,16 @@ import { useCabins } from './useCabins';
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  // Handle filter by discount FE-side
+  const filterValue = searchParams.get('discount') || 'all';
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0);
+  if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0);
 
   return (
     <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr' empty='No cabins found'>
@@ -20,7 +29,10 @@ function CabinTable() {
       </Table.Header>
 
       {/* Render Props Pattern */}
-      <Table.Body data={cabins} render={cabin => <CabinRow key={cabin.id} cabin={cabin} />} />
+      <Table.Body
+        data={filteredCabins}
+        render={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
+      />
     </Table>
   );
 }
