32a48577b8a577e5d2ae30d401c15f4a990d9900
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-23 08:59:39 +0300

    feat: ✨ Add Api-side filtering operations for bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter bookings by status
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings
+    queryKey: ['bookings', filter], // depend on filter value to refetch data
+    queryFn: () => getBookings({ filter })
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,16 @@
 import supabase from './supabase';
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from('bookings')
     .select(
       'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
     ); // select all columns from bookings table + data from related foreign tables
+
+  // Add filter to query
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
