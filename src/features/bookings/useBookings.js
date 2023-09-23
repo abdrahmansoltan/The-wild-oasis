a70a88fad3c49ece09aa13be5e3b3c89a5aec0c6
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-23 10:42:28 +0300

    feat: :sparkles: implement prefetching for bookings pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // Filter bookings by status
@@ -18,6 +20,7 @@ export function useBookings() {
   // Pagination
   const page = Number(searchParams.get('page')) || 1;
 
+  // Fetch bookings
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -26,6 +29,25 @@ export function useBookings() {
     queryKey: ['bookings', filter, sortBy, page], // depend on filter & sortBy value to refetch data
     queryFn: () => getBookings({ filter, sortBy, page })
   });
+
+  // Pre-fetching the next page
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount) {
+    // only pre-fetch if there is a next page
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+    });
+  }
+
+  // Pre-fetching the previous page
+  if (page > 1) {
+    // only pre-fetch if there is a previous page
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 })
+    });
+  }
 
   return { isLoading, bookings, count, error };
 }
