4b8868fb011dfda976bf57754e462ad699264db0
Abdelrahman Soltan <asoltan@instabug.com>
2023-09-26 22:29:33 +0300

    feat(Auth): :technologist: create auth user--data-getter service and use it in custom react query hook

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -9,3 +9,13 @@ export async function login({ email, password }) {
 
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession(); // Get the active session
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser(); // Get the user data from Supabase
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
